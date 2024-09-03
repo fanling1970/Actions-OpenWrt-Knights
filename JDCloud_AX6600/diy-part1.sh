1e8ed102dd07d945934cdb78adb5822f168a2904
fanling1970 <zghqidai2008@sina.com>
2024-09-04 02:56:33 +0800

    Update diy-part1.sh

--- JDCloud_AX6600/diy-part1.sh
+++ JDCloud_AX6600/diy-part1.sh
@@ -23,3 +23,10 @@ echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.defa
 echo >> feeds.conf.default
 echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
 echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
+
+# 科学上网插件
+#git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
+git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
