a7df5decdfcc9b72f579f91d1a8104b96425c2a1
fanling1970 <zghqidai2008@sina.com>
2024-09-05 07:27:48 +0800

    Update diy-part1.sh

--- immortalwrt-ax6600/diy-part1.sh
+++ immortalwrt-ax6600/diy-part1.sh
@@ -1,4 +1,5 @@
 
+
 #!/bin/bash
 #
 # https://github.com/P3TERX/Actions-OpenWrt
@@ -28,7 +29,7 @@ echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >
 #sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # 科学上网插件
-git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+#git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
+#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 #git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
