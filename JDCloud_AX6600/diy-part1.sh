ed3f7b71dd54a347f5d972e4151aa2a1e6c31ddb
fanling1970 <zghqidai2008@sina.com>
2024-09-06 21:38:23 +0800

    Update diy-part1.sh

--- JDCloud_AX6600/diy-part1.sh
+++ JDCloud_AX6600/diy-part1.sh
@@ -14,8 +14,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-git clone https://github.com/kenzok8/openwrt-packages package/kenzo
-git clone https://github.com/kenzok8/small package/small
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 #echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
 
 #echo >> feeds.conf.default
